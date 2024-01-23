b847b610943626ad12735a11412a2fb649d7e1d9
MuhammadYusuf0I <abduvasiyevmuhammadyusuf28@gmail.com>
2024-01-23 20:10:13 +0500

    homework

---
+++ index.js
@@ -0,0 +1,143 @@
+// 1-masal
+
+// let moon = +prompt(`Oyning raqamini kirgazing`);
+// let res = `Bunday oy mavjud emas`;
+// switch (n) {
+//   case 1:
+//     res = `Yanvar`;
+//     break;
+//   case 2:
+//     res = `Fevral`;
+//     break;
+//   case 3:
+//     res = `Mart`;
+//     break;
+//   case 4:
+//     res = `Aprel`;
+//     break;
+//   case 5:
+//     res = `May`;
+//     break;
+//   case 6:
+//     res = `Iyun`;
+//     break;
+//   case 7:
+//     res = `Iyul`;
+//     break;
+//   case 8:
+//     res = `Avgust`;
+//     break;
+//   case 9:
+//     res = `Sentabr`;
+//     break;
+//   case 10:
+//     res = `Oktabr`;
+//     break;
+//   case 11:
+//     res = `Noyabr`;
+//     break;
+//   case 12:
+//     res = `Dekabr`;
+//     break;
+//   default:
+//     res = `Bunday oy mavjud emas`;
+//     break;
+// }
+// console.log(res);
+
+// let year = +prompt(`Oyning raqamini kirgazing`);
+// let res$ = `Bunday fasil mavjud emas`;
+// switch (year) {
+//   case 1:
+//     res$ = `Qish`;
+//     break;
+//   case 2:
+//     res$ = `Qish`;
+//     break;
+//   case 3:
+//     res$ = `Bahor`;
+//     break;
+//   case 4:
+//     res$ = `Bahor`;
+//     break;
+//   case 5:
+//     res$ = `Bahor`;
+//     break;
+//   case 6:
+//     res$ = `Yoz`;
+//     break;
+//   case 7:
+//     res$ = `Yoz`;
+//     break;
+//   case 8:
+//     res$ = `Yoz`;
+//     break;
+//   case 9:
+//     res$ = `Kuz`;
+//     break;
+//   case 10:
+//     res$ = `Kuz`;
+//     break;
+//   case 11:
+//     res$ = `Kuz`;
+//     break;
+//   case 12:
+//     res$ = `Qish`;
+//     break;
+//   default:
+//     res$ = `Bunday fasil mavjud emas`;
+//     break;
+// }
+// console.log(res$);
+
+// 3-masal
+
+// let num1 = +prompt(`Sonni kiriting`);
+// for (let i = 2; i <= num1; i++) {
+//   let tub = true;
+
+//   for (let j = 2; j < i; j++) {
+//     if (i % j == 0) {
+//       tub = false;
+//       break;
+//     }
+//   }
+
+//   if (tub) {
+//     console.log(i);
+//   }
+// }
+
+// 4-masal
+
+// let number = +prompt("Inputlar soni");
+// let sum = 0;
+
+// for (let i = 0; i <= number; i++) {
+//   let num = +prompt(`${i}-sonni kiriting`);
+//   sum += num;
+// }
+// console.log(sum);
+
+// 5-masal
+
+// let num = +prompt(`Sonni kiriting`);
+// for (let i = num; i >= 1; i-=1) {
+//   console.log(i);
+// }
+
+// 6-masal
+
+// let number$ = +prompt(`Sonni kiriting`);
+// let Sum$=1;
+// for (let i = 1; i<=number$; i++){
+// Sum$=i*Sum$;
+// } console.log(Sum$)
+
+// 7-masal
+
+// let number1 =+prompt(`Sonni kiriting`);
+// let sum$=0
+// for(let i=1; i<=number1; i++) {
+//   sum$=sum$+i
+// }console.log(sum$);
